aa5c43ba2583ba46dc37c0db0ffd17d915f6107f
Usuario9342 <usuario934280@protonmail.com>
2020-08-15 22:24:38 -0300

    volviendo al ruedo

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Voy a ir progresando esto de a poco mientras estudio c++ y sus funciones de manejo de archivos. La idea es un pequeño sistema que desde un menú permita mantener una diminuta base de datos de empleados y que incluya su nombre, apellido, sueldo, y su locacion... Ciudad, pais, provincia, calle y numeracion. El menu tiene que permitir buscar dar de alta un empleado, borrarlo, buscarlo, enumerarlos todos, editar los datos de un empleado y por ultimo tiene que poder imprimir los datos de un empleado en un txt.
 
 Basicamente un CRUD en c++ con persistencia en archivos binarios.
+
+
+15/8/20
+
+Bueno, despues de varias semanas sin tener tiempo para joder con esto (tambien tengo que trabajar y evadir exitosamente la cuarentena medieval) empiezo a ordenar un poco el codigo y planear la interfase con el usuario.
+
+Por lo pronto creo la libreria "libreria.h" y apilo las funciones y definiciones ahi. Cuando hice la movida me saltaron errores por todos lados, ya que el nuevo archivo no leia las definiciones de los struct. La solucion que encontre para que por lo menos no aparecieran tantas luces rojas en vscode fue: hacer un #include en libreria.h y endosarle empleados.cpp que es donde estan las definiciones. Espero que ande porque aun no probe el codigo. Por otro lado, tengo que averiguar como usar colores para el menu asi no queda todo tan choto.
+
+Tengo que mejorar la funcion de busqueda y usarla para las funciones de borrado y listado individual.
--- empleados.cpp
+++ empleados.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 #include<fstream>
 #include <cstring>
+
+#include "libreria.h"
+
+
 using namespace std;
+using namespace libreria;
 
 typedef struct {
    char calle[30];
@@ -18,14 +23,7 @@ struct empleado {
    domicilio dir;
 };
 
-void cargarEmpleado(empleado *emp);
-void imprimirEmpleado(empleado emp);
-void leerArchivoBinario(empleado *leer);
-void escribirArchivoBinario(empleado emp);
-void borrarEmpleado(char apellido[]);
-int contarRegistros();
-int numeracionDeEmpleados();
-void imprimirArchivo();
+
 
 int main() {
 
@@ -55,7 +53,7 @@ int main() {
 
    //leerArchivoBinario(&leer);
 
-   imprimirArchivo();
+   //imprimirArchivo();
    
   
    //esto es la estructura mas basica que encontre para escribir y leer struct en binarios. Hay un problema si no usas elementos de un tamanio certero en el struct. Si le pones un "string" en lugar de un char[] no podes leer saltando registros de tamanio fijo. Voy a ver como se resuelve eso otro dia.
@@ -65,160 +63,4 @@ int main() {
    return 0;
 }
 
-//empiezo modularizando la carga, esta tiene que recibir un puntero porque retorna los valores a al struct
 
-void cargarEmpleado(empleado *emp){
-   std::cout<<"\nIngrese los datos del empleado: "<<endl;  
-   std::cout<<"nombre: ";
-   //strcpy(escribir.apellido, "pepe");
-   std::cin>>(*emp).nombre;
-   std::cout<<"apellido: ";
-   std::cin>>(*emp).apellido;
-   std::cout<<"calle: ";
-   std::cin>>(*emp).dir.calle;
-   std::cout<<"numero: ";
-   std::cin>>(*emp).dir.numero;
-   std::cout<<"ciudad: ";
-   std::cin>>(*emp).dir.ciudad;
-   std::cout<<"provincia: ";
-   std::cin>>(*emp).dir.provincia;
-   std::cout<<"pais: ";
-   std::cin>>(*emp).dir.pais;
-   (*emp).numeroEmp = numeracionDeEmpleados();
-}
-
-//la impresion de el registro la mando a esta funcion. paso el parametro por copia porque solo hace una salida a pantalla.
-
-void imprimirEmpleado(empleado emp){
-   std::cout<<"------------------"<< endl;
-   std::cout<<emp.apellido<< endl;
-   std::cout<<emp.nombre<< endl;
-   std::cout<<emp.dir.calle<< endl;
-   std::cout<<emp.dir.ciudad<< endl;
-   std::cout<<emp.dir.provincia<< endl;
-   std::cout<<emp.dir.pais<< endl;
-   std::cout<<emp.numeroEmp<<endl;
-   std::cout<<"------------------"<< endl;
-}
-
-//funcion de lectura secuencial del archivo binario, esta funcion lista TODOS los registros.
-
-void leerArchivoBinario(empleado *leer){
-   //di muchas vueltas para lograr la lectura secuencial. Al final el algoritmo es este, una lectura previa y que entre a un while que constate que no este en el EOF, la gracia es que la comprobacion va en la segunda lectura.
-   ifstream leerArchivo("archivo.dat", ios::binary );     
-   leerArchivo.read((char*)leer, sizeof(empleado));
-   
-   while (!leerArchivo.eof())
-   {
-        imprimirEmpleado(*leer);
-        leerArchivo.read((char*)leer, sizeof(empleado));
-   }
-   leerArchivo.close();
-}
-
-//funcion de escritura del archivo binario, esta funcion escribe solo UN registro.
-
-void escribirArchivoBinario(empleado emp){
-   ofstream escribirArchivo("archivo.dat", ios::binary | ios::app);
-   escribirArchivo.write((char*)&emp, sizeof(empleado));
-   escribirArchivo.close();
-}
-
-//funcion de borrado de un registro. 
-
-void borrarEmpleado(char apellido[]){
-   empleado emp;
-   ofstream escrituraTemp ("temp.dat", ios::binary);
-   ifstream lecturaArch ("archivo.dat", ios::binary);
-
-   //preparo la lectura del binario y inicio un archivo temporal para hacer la recopilacion de los registros que no quiero descartar. Tendria que buscar si esto mismo se puede hacer sin salir de memoria, usando un buffer y no un archivo. Tambien tengo que ampliar el struct empleado para que registre su numero de empleado. y que una busqueda por nombre o lo que quiera retorne el numero de empleado.
-
-   //meter como condicion la instruccion de lectura no se me habia ocurrido, gracias internet.
-
-   while(lecturaArch.read((char*)&emp,sizeof(empleado))){
-     if(strcmp(emp.apellido, apellido)) {
-        //uso la propiedad aplellido porque no tengo lista la funcion que numere a los empleados. 
-        escrituraTemp.write((char*)&emp,sizeof(empleado));
-     }
-
-   }
-   lecturaArch.close();
-   escrituraTemp.close();
-
-   remove("archivo.dat");
-   rename("temp.dat","archivo.dat");
-}
-
-//funcion de busqueda de empleado, le ingresamos un string y nos retorna el numero de empleado, si no existe nos retorna 
-
-int buscarEmpleado(char apellido[]){
-   empleado emp;
-   ifstream lecturaArch ("archivo.dat", ios::binary);
-
-   while(lecturaArch.read((char*)&emp,sizeof(empleado))){
-     if(strcmp(emp.apellido, apellido)) {
-        //uso la propiedad apellido porque no tengo lista la funcion que numere a los empleados. 
-        //retorno la posicion del registro o su numero de empleado (cuando termine la funcion de numerar cliente)
-         return emp.numeroEmp;
-     } else {
-        cout<<"el empleado no existe";
-     }
-
-   }
-   lecturaArch.close();
-
-}
-
-//funcion para numerar los empleados.
-
-int numeracionDeEmpleados(){
-   //cuanto los registros y le sumo el actual, liego ingreso ese numero en el registro a escribir;
-   int total = contarRegistros();
-   return total++;
-    
-}
-
-//funcion para contar todos los registros que tenga en el archivo.
-
-int contarRegistros(){
-   int acumulador = 0;
-   empleado leer;
-   ifstream lecturaArch ("archivo.dat", ios::binary);
-   
-   while(lecturaArch.read((char*)&leer, sizeof(empleado))){
-      acumulador++;
-   }
-   lecturaArch.close();
-   return acumulador;
-}
-
-//funcion para imprimir todo el archivo en un txt.
-
-void imprimirArchivo(){
-   ifstream binario;
-   binario.open("archivo.dat", ios::binary);
-   ofstream texto;
-   texto.open("empleados.txt");
-    if (!binario || !texto){
-        cout << "No se pudo crear el archivo" << endl;
-        
-    }
-
-    empleado emp;             
-    
-    while (binario.read((char*)&emp, sizeof(empleado))){
-        texto<<"-----------------------"<<endl;
-        texto<<"Nombre: "<<emp.nombre<<endl;
-        texto<<"Apellido: "<<emp.apellido<<endl;
-        texto<<"Calle: "<<emp.dir.calle<<endl;
-        texto<<"Numero: "<<emp.dir.numero<<endl;
-        texto<<"Ciudad: "<<emp.dir.ciudad<<endl;
-        texto<<"Provincia: "<<emp.dir.provincia<<endl;
-        texto<<"Pais: "<<emp.dir.pais<<endl;
-        texto<<"-----------------------"<<endl;
-    }
-
-    
-    binario.close();
-    texto.close();
-}
